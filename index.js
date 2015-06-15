5b34dbe030649ae7e2d8b3e09ad6cc94d271486a
tormozz48 <tormozz48@gmail.com>
2015-06-15 22:54:04 +0300

    Add index.js file for suitable task plug-in

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.tasks = {
+    PageHeaderTitle: require('./lib/tasks/page-header-title'),
+    PageHeaderMeta: require('./lib/tasks/page-header-meta'),
+    PageBreadcrumbs: require('./lib/tasks/page-breadcrumbs'),
+    PageSearchMeta: require('./lib/tasks/page-search-meta')
+};
+
